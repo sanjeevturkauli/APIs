2df84accb79b8062368548d56d77cbd4acd2bdc7
Sanjeev Kumar <radhe1310395@gmail.com>
2025-12-07 20:37:43 +0530

    Add Test Endpoint

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/test', function (Request $request) {
+    return response()->json([
+        'status'=>'success',
+        'statusCode'=>200,
+        'message'=>"API successfuly tested ok."
+    ]);
+});
